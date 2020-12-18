22fc91411186e0bfd44f780e683d790471c12a4f
David Gilkeson <davidgilkeson8@gmail.com>
2020-12-18 00:03:55 +0000

    Fixed  Post page

--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import sanityClient from '../client.js';
 import imageUrlBuilder from '@sanity/image-url';
@@ -12,33 +12,34 @@ function urlFor(source) {
 export default function SinglePost() {
   const [singlePost, setSinglePost] = useState(null);
   const { slug } = useParams();
+
   useEffect(() => {
     sanityClient
       .fetch(
         `*[slug.current == "${slug}"]{
-title,
-_id,
-slug,
-mainImage{
-  asset->{
-    _id,
-    url
-  }
-},
-body,
-"name": author->name,
-"authorImage: author->image"
-}`
+        title,
+        _id,
+        slug,
+        mainImage{
+          asset->{
+            _id,
+              url
+            }
+          },
+          body,
+          "name": author->name,
+          "authorImage": author->image
+        }`
       )
       .then((data) => setSinglePost(data[0]))
       .catch(console.error);
   }, [slug]);
 
-  if (!SinglePost) return <div>Loading...</div>;
+  if (!singlePost) return <div>Loading...</div>;
 
   return (
     <main className="bg-gray-200 min-h-screen p-12">
-      <article className="container shadow-lg mx-auto bg-gree-100 rounded-lg ">
+      <article className="container shadow-lg mx-auto bg-green-100 rounded-lg">
         <header className="relative">
           <div className="absolute h-full w-full flex items-center justify-center p-8">
             <div className="bg-white bg-opacity-75 rounded p-12">
@@ -51,10 +52,10 @@ body,
                   alt={singlePost.name}
                   className="w-10 h-10 rounded-full"
                 />
+                <p className="cursive flex items-center pl-2 text-2xl">
+                  {singlePost.name}
+                </p>
               </div>
-              <p className="cursive flex items-center pl-2 text-2xl">
-                {singlePost.name}
-              </p>
             </div>
           </div>
           <img
@@ -69,7 +70,7 @@ body,
             blocks={singlePost.body}
             projectId="60jhatv9"
             dataset="production"
-          ></BlockContent>
+          />
         </div>
       </article>
     </main>
