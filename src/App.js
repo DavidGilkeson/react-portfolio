9440d1f707d8c5eeb82e42359e0be676fd1add74
David Gilkeson <davidgilkeson8@gmail.com>
2020-12-18 18:23:18 +0000

    App js updated

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import { BrowserRouter, Route, Switch} from "react-router-dom";
-import Home from "./components/Home";
-
-import About from "./components/About"
-import SinglePost from "./components/SinglePost"
-import Post from "./components/Post"
-import Project from "./components/Project"
-import NavBar from "./components/NavBar"
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import About from './components/About';
+import Post from './components/Post';
+import Home from './components/Home';
+import NavBar from './components/NavBar';
+import Project from './components/Project';
+import SinglePost from './components/SinglePost';
 
 function App() {
   return (
     <BrowserRouter>
-    <NavBar />
+      <NavBar />
       <Switch>
-        <Route component={Home} path='/' exact /> 
-        <Route component={About} path='/about' /> 
-        <Route component={SinglePost} path='/post/:slug' /> 
-        <Route component={Post} path='/post' /> 
-        <Route component={Project} path='/project' /> 
+        <Route component={Home} path="/" exact />
+        <Route component={About} path="/about" />
+        <Route component={SinglePost} path="/post/:slug" />
+        <Route component={Post} path="/post" />
+        <Route component={Project} path="/project" />
       </Switch>
     </BrowserRouter>
   );
